b84264d93efebc0a80654a87d0d1e5aaec76abbc
Hossein <test@example.com>
2026-09-11 17:41:16 +0330

    Upgrade to MySQL database

--- database.py
+++ database.py
@@ -32,3 +32,279 @@ def create_table():
 
 
 create_table()
+
+
+def add_student(name, age, score):
+    cursor = db.cursor()
+
+    query = """
+    INSERT INTO students (name, age, score)
+    VALUES (%s, %s, %s)
+    """
+
+    values = (name, age, score)
+
+    cursor.execute(query, values)
+    db.commit()
+    cursor.close()
+
+
+def show_students():
+    cursor = db.cursor()
+
+    query = "SELECT * FROM students"
+
+    cursor.execute(query)
+
+    students = cursor.fetchall()
+
+    for student in students:
+        print(
+            f"ID: {student[0]}, name: {student[1]}, age: {student[2]}, score: {student[3]}")
+
+    cursor.close()
+
+
+def search_student(search_value, search_by):
+    cursor = db.cursor()
+
+    if search_by == "id":
+        query = """
+        SELECT * FROM students
+        WHERE id = %s
+        """
+        values = (search_value,)
+
+    elif search_by == "name":
+        query = """
+        SELECT * FROM students
+        WHERE name = %s
+        """
+        values = (search_value,)
+
+    else:
+        print("Invalid search type.")
+        cursor.close()
+        return
+
+    cursor.execute(query, values)
+
+    students = cursor.fetchall()
+
+    if students:
+        for student in students:
+            print(
+                f"ID: {student[0]}, "
+                f"name: {student[1]}, "
+                f"age: {student[2]}, "
+                f"score: {student[3]}"
+            )
+    else:
+        print("Student not found.")
+
+    cursor.close()
+
+
+def delete_student(id):
+    cursor = db.cursor()
+
+    query = """
+    DELETE FROM students
+    WHERE id = %s
+    """
+
+    values = (id,)
+
+    cursor.execute(query, values)
+
+    if cursor.rowcount > 0:
+        db.commit()
+        print("Student deleted successfully.")
+    else:
+        print("Student not found.")
+
+    cursor.close()
+
+
+def statistics():
+    cursor = db.cursor()
+
+    query = """
+    SELECT 
+        COUNT(*),
+        MAX(score),
+        MIN(score),
+        AVG(score)
+    FROM students
+    """
+
+    cursor.execute(query)
+
+    result = cursor.fetchone()
+
+    if result[0] == 0:
+        cursor.close()
+        return "No information has been recorded!"
+
+    number_of_students = result[0]
+    highest_score = result[1]
+    lowest_score = result[2]
+    average_of_scores = result[3]
+
+    query = """
+    SELECT COUNT(*)
+    FROM students
+    WHERE score >= 10
+    """
+
+    cursor.execute(query)
+    passes = cursor.fetchone()[0]
+
+    query = """
+    SELECT COUNT(*)
+    FROM students
+    WHERE score < 10
+    """
+
+    cursor.execute(query)
+    fails = cursor.fetchone()[0]
+
+    query = """
+    SELECT id, name, score
+    FROM students
+    ORDER BY score DESC
+    LIMIT 1
+    """
+
+    cursor.execute(query)
+    best_student = cursor.fetchone()
+
+    query = """
+    SELECT id, name, score
+    FROM students
+    ORDER BY score ASC
+    LIMIT 1
+    """
+
+    cursor.execute(query)
+    worst_student = cursor.fetchone()
+
+    cursor.close()
+
+    return f"""
+    Statistics:
+    number_of_students: {number_of_students}
+    highest_score: {highest_score}
+    lowest_score: {lowest_score}
+    average_of_scores: {average_of_scores}
+    count of passes: {passes}
+    count of failers: {fails}
+    best student: ID {best_student[0]}, name: {best_student[1]}, score: {best_student[2]}
+    worst student: ID {worst_student[0]}, name: {worst_student[1]}, score: {worst_student[2]}
+    """
+
+
+def edit_student(id, new_name, new_age, new_score):
+    cursor = db.cursor()
+
+    query = """
+    UPDATE students
+    SET name = %s,
+        age = %s,
+        score = %s
+    WHERE id = %s
+    """
+
+    values = (new_name, new_age, new_score, id)
+
+    cursor.execute(query, values)
+
+    if cursor.rowcount > 0:
+        db.commit()
+        print("Student Updated!")
+    else:
+        print("Student not found")
+
+    cursor.close()
+
+
+def sort_students():
+    cursor = db.cursor()
+
+    choice = input("""
+        Sort by score:
+        1. Highest to Lowest
+        2. Lowest to Highest
+        Choose:
+        """
+                   )
+
+    if choice == "1":
+        query = """
+        SELECT * FROM students
+        ORDER BY score DESC
+        """
+    elif choice == "2":
+        query = """
+        SELECT * FROM students
+        ORDER BY score ASC
+        """
+    else:
+        print("Invalid choice.")
+        cursor.close()
+        return
+
+    cursor.execute(query)
+    students = cursor.fetchall()
+
+    if students:
+        print("The Students:")
+
+        for student in students:
+            print(
+                f"ID: {student[0]}, name: {student[1]}, age: {student[2]}, score: {student[3]}")
+    else:
+        print("Students list is empty.")
+
+    cursor.close()
+
+
+def search_by_score(score):
+    cursor = db.cursor()
+
+    query = """
+    SELECT * FROM students
+    WHERE score >= %s
+    """
+
+    values = (score,)
+
+    cursor.execute(query, values)
+    students = cursor.fetchall()
+
+    if students:
+        for student in students:
+            print(
+                f"ID: {student[0]}, name: {student[1]}, age: {student[2]}, score: {student[3]}")
+    else:
+        print("No student found.")
+
+    cursor.close()
+
+
+def delete_all_students():
+    cursor = db.cursor()
+
+    query = """
+    DELETE FROM students
+    """
+
+    cursor.execute(query)
+
+    if cursor.rowcount > 0:
+        db.commit()
+        print("All students deleted successfully.")
+    else:
+        print("Students list is empty.")
+
+    cursor.close()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-students = []
+import database
 
 
-# maghz ertebati barname
+# Program Communication Brain
 def main():
     while True:
         user_selection = show_menu()
@@ -11,192 +11,84 @@ def main():
             age = int(input("age: "))
             score = float(input("score: "))
 
-            add_student(name, age, score)
+            database.add_student(name, age, score)
+            print("Student added!")
 
         elif user_selection == 2:
-            show_students()
+            database.show_students()
 
         elif user_selection == 3:
-            who_to_search = input('''
-            Enter the name of student: 
-            ''')
-            print(search_student(who_to_search))
+            search_by = input("Search by ID or name: ").lower()
+
+            if search_by == "id":
+                search_value = int(input("Enter student ID: "))
+                database.search_student(search_value, "id")
+
+            elif search_by == "name":
+                search_value = input("Enter student name: ")
+                database.search_student(search_value, "name")
+
+            else:
+                print("Invalid search type.")
 
         elif user_selection == 4:
-            who_to_delete = input("Enter the name of student to remove: ")
-            print(delete_student(who_to_delete))
+            student_id = int(input("Enter student ID to delete: "))
+            database.delete_student(student_id)
 
         elif user_selection == 5:
-            print(statistics())
+            print(database.statistics())
 
         elif user_selection == 6:
-            print('''
+            print("""
             good bye!!!
-            ''')
+            """)
             break
 
         elif user_selection == 7:
-            who_to_edit = input("Enter the name of student you want to edit: ")
-            edit_student(who_to_edit)
+            student_id = int(input("Enter student ID you want to edit: "))
+            new_name = input("Enter the new name: ")
+            new_age = int(input("Enter the new age: "))
+            new_score = float(input("Enter the new score: "))
+
+            database.edit_student(
+                student_id,
+                new_name,
+                new_age,
+                new_score
+            )
 
         elif user_selection == 8:
-            sort_students()
+            database.sort_students()
 
         elif user_selection == 9:
-            score = int(input("Enter the minimum score: "))
-            search_by_score(score)
+            score = float(input("Enter the minimum score: "))
+            database.search_by_score(score)
 
         elif user_selection == 10:
-            delete_all_students()
+            database.delete_all_students()
 
         else:
             print("Please enter a valid number.")
 
 
-# menuei ke karbar hengam RUN mibine
+# The menu that the user sees
 def show_menu():
-    print(
-        '''
-        ======== Student Management ========
-        1. Add Student
-        2. Show Students
-        3. Search Student
-        4. Delete Student
-        5. Statistics
-        6. Exit
-        7. Edit Student
-        8. Sort Students 
-        9. Search By Score
-        10. Delete All Students
-        ''')
+    print("""
+    ======== Student Management ========
+    1. Add Student
+    2. Show Students
+    3. Search Student
+    4. Delete Student
+    5. Statistics
+    6. Exit
+    7. Edit Student
+    8. Sort Students
+    9. Search By Score
+    10. Delete All Students
+    """)
 
     user_selection = int(input("Choose: "))
     return user_selection
-
-
-# karhaei ke system bayad anjam bede
-def add_student(name, age, score):
-    for student in students:
-        if name == student[0]:
-            print("Student already exists.")
-            break
-    else:
-        student = [name, age, score]
-        students.append(student)
-        print('''
-                    student added!
-                    ''')
-
-
-def show_students():
-    if len(students) != 0:
-        print("The Students: ")
-        for index, student in enumerate(students):
-            print(
-                f"{index + 1}. name: {student[0]}, age: {student[1]}, score: {student[2]}")
-    else:
-        print("Student list is empty.")
-
-
-def search_student(name):
-    for student in students:
-        if student[0] == name:
-            return f'''Found!
-            name: {student[0]}, age: {student[1]}, score: {student[2]}'''
-    else:
-        return "Student not found."
-
-
-def delete_student(name):
-    for index, student in enumerate(students):
-        if student[0] == name:
-            students.pop(index)
-            return "Deleted Successfully"
-    else:
-        return "Student not found."
-
-
-def statistics():
-    if len(students) != 0:
-        number_of_students = len(students)
-        student_scores = []
-        total_of_scores = 0
-        passes_scores = []
-        failed_scores = []
-
-        for student in students:
-            student_scores.append(student[2])
-            total_of_scores += student[2]
-            if student[2] >= 10:
-                passes_scores.append(student[2])
-            else:
-                failed_scores.append(student[2])
-
-        highest_score = max(student_scores)
-        for student in students:
-            if highest_score == student[2]:
-                name_of_best_student = student[0]
-
-        lowest_score = min(student_scores)
-        for student in students:
-            if lowest_score == student[2]:
-                name_of_worst_student = student[0]
-
-        average_of_scores = total_of_scores / len(students)
-
-        return f'''Statistics:
-        number_of_students: {number_of_students}, highest_score: {highest_score}, lowest_score: {lowest_score}, average_of_scores: {average_of_scores},
-        count of passes: {len(passes_scores)}, count of failers: {len(failed_scores)}, 
-        name of best student: {name_of_best_student}, name of worst student: {name_of_worst_student}
-        '''
-    else:
-        return "No information has been recorded!"
-
-
-def edit_student(name):
-    for student in students:
-        if student[0] == name:
-            new_name = input("Enter the new name: ")
-            new_age = int(input("Enter the new age: "))
-            new_score = float(input("Enter the new score: "))
-
-            student[0] = new_name
-            student[1] = new_age
-            student[2] = new_score
-
-            print("Student edited!!!")
-            break
-    else:
-        print("Student not found!!!")
-
-
-def sort_students():
-    if len(students) != 0:
-        students.sort()
-        print("The Students: ")
-        for index, student in enumerate(students):
-            print(
-                f"{index + 1}. name: {student[0]}, age: {student[1]}, score: {student[2]}")
-    else:
-        print("Students list is empty.")
-
-
-def search_by_score(score):
-    if len(students) != 0:
-        found = False
-        for student in students:
-            if student[2] >= score:
-                print(f"{student[0]}, {student[1]}, {student[2]}")
-                found = True
-        if found == False:
-            print("No student found.")
-    else:
-        print("Students list is empty.")
-
-
-def delete_all_students():
-    students.clear()
-    print("All Students Deleted!!")
 
 
 main()
